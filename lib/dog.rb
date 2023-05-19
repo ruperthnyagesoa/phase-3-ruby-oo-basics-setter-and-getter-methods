f09e6be9eaf1ca45cccdb4c229f65161e64071a7
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-05-19 03:36:51 +0300

    Completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,9 @@
 class Dog
-
+    attr_accessor :name, :breed
+  
+    def initialize
+      @name = ""
+      @breed = ""
+    end
 end
+  
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-
+    attr_accessor :name, :job
+  
+    def initialize
+      @name = ""
+      @job = ""
+    end
 end
